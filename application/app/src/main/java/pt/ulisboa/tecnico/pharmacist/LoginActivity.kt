1598cf6a29c4c763c3d6194764de3e05cadba746
SaraBuu <sara.aguincha@gmail.com>
2024-05-05 00:05:24 +0100

    Started login UI logic

---
+++ application/app/src/main/java/pt/ulisboa/tecnico/pharmacist/LoginActivity.kt
@@ -0,0 +1,33 @@
+package pt.ulisboa.tecnico.pharmacist
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// possible change this to jetpack compose
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+    }
+
+    // when clicking the guest button redirect to mapsActivity
+    fun guestButtonClick(view: View?) {
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
+}

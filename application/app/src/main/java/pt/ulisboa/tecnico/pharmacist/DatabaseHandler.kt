75562b4d7b0ced9bf47f886ddd2281e4045387f9
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2024-05-23 02:45:59 +0100

    Start of sqlite logic

---
+++ application/app/src/main/java/pt/ulisboa/tecnico/pharmacist/DatabaseHandler.kt
@@ -0,0 +1,93 @@
+package pt.ulisboa.tecnico.pharmacist
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        private const val DATABASE_NAME = "pharmacistCache.db"
+        private const val DATABASE_VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+
+        val PHARMACIES = """
+            CREATE TABLE pharmacies (
+            pharmacy_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            name TEXT NOT NULL,
+            address TEXT NOT NULL,
+            latitude REAL NOT NULL,
+            longitude REAL NOT NULL
+        );
+        """.trimIndent()
+
+        val FAVORITE_PHARMACIES = """
+            CREATE TABLE favorite_pharmacies (
+                favorite_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                user_id INTEGER NOT NULL,
+                pharmacy_id INTEGER NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (pharmacy_id) REFERENCES pharmacies(pharmacy_id) ON DELETE CASCADE ON UPDATE CASCADE
+            );
+        """.trimIndent()
+
+        val MEDICINE = """
+            CREATE TABLE medicine (
+                medicine_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                name TEXT NOT NULL,
+                purpose TEXT NOT NULL
+            );
+        """.trimIndent()
+
+
+        val MEDICINE_STOCK = """
+            CREATE TABLE medicine_stock (
+                medicine_stock_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                pharmacy_id INTEGER NOT NULL,
+                medicine_id INTEGER NOT NULL,
+                quantity INTEGER NOT NULL,
+                FOREIGN KEY (pharmacy_id) REFERENCES pharmacies(pharmacy_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (medicine_id) REFERENCES medicine(medicine_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                UNIQUE (pharmacy_id, medicine_id)
+            );
+        """.trimIndent()
+        db.execSQL(PHARMACIES)
+        db.execSQL(FAVORITE_PHARMACIES)
+        db.execSQL(MEDICINE_STOCK)
+        db.execSQL(MEDICINE)
+    }
+
+    // For testing purposes
+    fun foo() {
+        val db = this.writableDatabase
+
+        val query = """
+            insert into pharmacies (name, address, latitude, longitude)
+                values ('Farmácia Rossio', 'Rua Augusta 1, 1250-162 Lisboa', 38.712790, -9.137420);
+        """.trimIndent()
+
+        val response = db.execSQL(query)
+
+        val cursor = db.query("pharmacies", null, null, null, null, null, null)
+
+        with(cursor) {
+            while (moveToNext()) {
+                val name = getString(getColumnIndexOrThrow("name"))
+                val address = getString(getColumnIndexOrThrow("address"))
+                val latitude = getDouble(getColumnIndexOrThrow("latitude"))
+                val longitude = getDouble(getColumnIndexOrThrow("longitude"))
+                println(name)
+                println(address)
+                println(latitude)
+                println(longitude)
+            }
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS mytable")
+        onCreate(db)
+    }
+}
